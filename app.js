d6ae1c79ea1df386dade58f8aad5c0780f789a27
Dawa Sherpa <dasherpa7@gmail.com>
2016-10-05 19:52:57 -0400

    in middle of refactoring detect edges functions

--- app.js
+++ app.js
@@ -1,41 +1,72 @@
 $(function() {
   console.log('dom loaded');
 
-  var $spaceShip = $('#spaceShip');
-  var $asteroid = $('.asteroid');
-  var direction = true;
-  var $leftBorder = $asteroid.position().left;
-  // debugger
-  var $rightBorder = window.innerWidth - 50;
+  if ($('body').is('.gamePage')) {
+    var $spaceShip = $('#spaceShip');
+    var $asteroid = $('.asteroid');
 
-  // if ($('body').is('.gamePage')) setInterval(moveAsteroid, 1);
+    // Asteroid coordination
+    var direction = true;
+    var $leftBorder = 0;
+    var $rightBorder = window.innerWidth - 50;
+    // find position of asteroid in number (not pixels) to compare with window.innerWidth
+    var $asteroidPosition = $asteroid.css('left').match(/[0-9]+/);
+
+    // setInterval(moveAsteroid, 1);
+  };
 
 // <<<<------------------- Functions ------------------->>>>
+
+function isCorner() {
+// debugger
+ // || $asteroidPosition <= $leftBorder
+
+  // if ($asteroidPosition > $rightBorder) {
+    if (parseInt($asteroidPosition[0]) > $rightBorder) {
+// debugger
+    console.log('TRUE! corner');
+    return true;
+  } else {
+    console.log('FALSE! corner');
+    return false;
+  }
+}
+
+function changeDirections() {
+  // update boolean value to signal change in direction in moveAsteroid()
+  direction = !direction;
+  console.log('CHANGING DIRECTIONS!');
+}
+
   function moveAsteroid() {
 // debugger
-
     if (direction) {
       $asteroid.css("left", "+=1px");
     } else {
       $asteroid.css("left", "-=1px");
     }
 
-    isCorner();
-  }
-
-  function isCorner() {
-    // find position of asteroid in number (not pixels) to compare with window.innerWidth
-    var $asteroidPosition = $asteroid.css('left').match(/[0-9]+/);
-    if ($asteroidPosition >= $rightBorder || $asteroidPosition <= $leftBorder) {
-      // update boolean value to signal change in direction in moveAsteroid()
-      direction = !direction;
-      console.log('CHANGING DIRECTIONS!');
+    if (isCorner()) {
+      console.log('is corner!');
+      changeDirections();
     }
   }
 
-// use switch case to register up / down left right key movements
+
+
 // edge detection to prevent movement when spaceship hits edges
 
+
+
+
+
+
+
+
+
+
+
+// move spaceship commands
   $('body').on('keydown', function(e) {
     switch (e.which) {
       case 37:
@@ -80,11 +111,12 @@ $(function() {
 
 
 
+// Goals
 
 
 
 
-
+// recode using OOP
 
 
 
--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 // sampe js code
 
-
 switch (this.which) {
   case 37:
   console.log('chose 37');
