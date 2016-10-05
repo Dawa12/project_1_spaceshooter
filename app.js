7babae9b60f6514c03809903335f98292fbbd0d6
Dawa Sherpa <dasherpa7@gmail.com>
2016-10-04 20:27:53 -0400

    created moving asteroid

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
 $(function() {
   console.log('dom loaded');
-})
+  var $asteroid = $('.asteroid');
+  // setInterval(moveAsteroid, 300);
+
+// Functions ------------------->>>>
+  function moveAsteroid() {
+
+    $asteroid.css({
+      "position": "absolute",
+      "left": "+=30px",
+    })
+
+    isCorner();
+    // console.log('just moved!');
+  }
+
+  function isCorner() {
+    if ($asteroid.css('left') >= window.innerWidth) {
+      $asteroid.css('left', '0');
+      console.log('STOPPPED');
+    }
+
+    console.log('isCorner called!  left:', $asteroid.css('left'));
+      // else if ()
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
